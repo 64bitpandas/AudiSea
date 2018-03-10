a1b338bea272b48590ba0e6f767c51bcbdf2135d
alexwaitz <alexanderjwaitz@gmail.com>
2018-03-10 15:53:55 -0800

    added gradient sky
    added an a-frame extension

--- src/views/Index.js
+++ src/views/Index.js
@@ -8,18 +8,12 @@ class Index extends Component {
                     <a-assets>
                         <img id="boxTexture" src="https://i.imgur.com/mYmmbrp.jpg"/>
                     </a-assets>
-                    
-                    <a-box position="0 2 -5" rotation="0 45 45" scale="2 2 2" src="#boxTexture">
-                        <a-animation attribute="position" direction="alternate" dur="2000" repeat="indefinite"
-                        to="0 2.2 -5"></a-animation>
-                        <a-animation attribute="rotation" begin="click" dur="2000" to="360 405 45"></a-animation>
-                        <a-animation attribute="scale" begin="mouseenter" dur="300" to="2.3 2.3 2.3"></a-animation>
-                        <a-animation attribute="scale" begin="mouseleave" dur="300" to="2 2 2"></a-animation>
-                    </a-box>
+
+                    <a-gradient-sky material="shader: gradient; topColor: 191 96 255; bottomColor: 66 134 244;"></a-gradient-sky>
 
                     <a-text value="This is example text" color="red" position="-1.5 0.7 -3" scale="1.5 1.5 1.5"></a-text>
 
-                    <a-ocean width="50" depth="50" density="40"></a-ocean>
+                    <a-ocean width="100" depth="100" density="90" color="#4286f4"></a-ocean>
                     
                     <a-camera position="0 0 0">
                         <a-cursor color="red"></a-cursor>
