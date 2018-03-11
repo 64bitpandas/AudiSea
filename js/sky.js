0e78b9589551777f989687faa26bb66de9cc36b4
Ben Cuan <ben@bananiumlabs.com>
2018-03-11 01:55:18 -0800

    CLOUDS !!!

--- js/sky.js
+++ js/sky.js
@@ -1,6 +1,18 @@
 var rollingAvg = new Array(100).fill(0);
+for(var x = -5; x < 5; x++)
+    for(var z = -5; z < 5; z++) {
+        var cloud = document.createElement("a-entity");
+        var obj = document.createAttribute("obj-model");
+            obj.value = "obj: #cloud; mtl: #cloud-mtl";
+        var position = document.createAttribute("position");
+        position.value = Math.random() * x * 10 + ' ' + 8 + ' ' + Math.random() * z * 10;
+        cloud.setAttributeNode(obj);
+        cloud.setAttributeNode(position);
+
+        document.getElementById('scene').appendChild(cloud);
+    }
+
 AFRAME.registerComponent('sky', {
-    
    
     schema: {
         analyserEl: {type: 'selector'},
@@ -27,6 +39,8 @@ AFRAME.registerComponent('sky', {
 
         var avg = rollingAvg.reduce(function (a, b) { return a + b; }) / rollingAvg.length;
         // console.log(avg);
+        var clouds = document.getElementsByClassName("cloud");
+
         skyEl.components.material.material = new THREE.MeshBasicMaterial();
        
     }
