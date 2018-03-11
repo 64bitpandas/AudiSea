2d827153a372de00c998ad119f2b53415c100c6e
alexwaitz <alexanderjwaitz@gmail.com>
2018-03-10 16:09:08 -0800

    remove wasd controls
    looking around is still enabled, but you can't walk anymore

--- src/views/Index.js
+++ src/views/Index.js
@@ -11,12 +11,10 @@ class Index extends Component {
 
                     <a-gradient-sky material="shader: gradient; topColor: 191 96 255; bottomColor: 66 134 244;"></a-gradient-sky>
 
-                    <a-text value="This is example text" color="red" position="-1.5 0.7 -3" scale="1.5 1.5 1.5"></a-text>
-
                     <a-ocean width="100" depth="100" density="90" color="#4286f4"></a-ocean>
-                    
-                    <a-camera position="0 0 0">
-                        <a-cursor color="red"></a-cursor>
+
+                    <a-camera wasd-controls-enabled="false" position="0 0 0">
+                        <a-cursor color="#05ffa1"></a-cursor>
                     </a-camera>
                 </a-scene>
             </div>
