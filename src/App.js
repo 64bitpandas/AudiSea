8a32e350a9ae0a636c229f3b20dc879d7bf5a5b0
alexwaitz <alexanderjwaitz@gmail.com>
2018-03-10 19:48:37 -0800

    added dropzone & font
    added a global font and a component for uploading files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react"
 import { Route } from "react-router-dom"
 import Index from "./views/Index"
-require('aframe-extras');
+import "./css/App.css"
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <link href="https://fonts.googleapis.com/css?family=Nanum+Gothic" rel="stylesheet"/>
         <Route exact path="/" component={Index}/>
       </div>
     )
